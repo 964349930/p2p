7c9d34955286594e0ec51bfb3456c454274ee68f
zhang <964349930@qq.com>
2014-09-03 09:58:18 +0800

    小修改
    小修改

--- app/Runtime/Cache/Admin/9e7f1214d7e23f27ccab23ddd8ad9e19.php
+++ app/Runtime/Cache/Admin/9e7f1214d7e23f27ccab23ddd8ad9e19.php
@@ -54,14 +54,11 @@
           <?php else: ?>
           <?php if(is_array($list)): $i = 0; $__LIST__ = $list;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$vo): $mod = ($i % 2 );++$i;?><tr>
                 <td><input type="checkbox" name="id[]" value="<?php echo ($vo["id"]); ?>"></td>
+                <td><?php echo ($vo["id"]); ?></td>
                 <td><?php echo ($vo["name"]); ?></td>
-                <td><?php echo ($vo["flag"]); ?></td>
-                <td><?php echo ($vo["sort"]); ?></td>
-                <td><?php echo (date("Y-m-d H:i", $vo["time_modify"])); ?></td>
-                <td>
-                    <a href="<?php echo U('Admin/Article/info',array('id'=>$vo[id]));?>" class="mr10">编辑</a>
-                    <a href="<?php echo U('Admin/Article/del',array('id'=>$vo[id]));?>">删除</a>
-                </td>
+                <td><?php echo ($vo[""]); ?></td>
+                <td><?php echo ($vo[""]); ?></td>
+                <td><?php echo ($vo[""]); ?></td>
               </tr><?php endforeach; endif; else: echo "" ;endif; endif; ?>
         </tbody>
       </table>
